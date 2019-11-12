05be6caade6a84f987c250b89b52bd76047e0f3e
Ankit Chaudhary <my007id@gmail.com>
2019-11-12 09:57:26 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,16 @@ using namespace std;
 
 int main() {
 
-
-  cout << "Hello, World!" << std::endl;
-
   Publisher publisher;
   Subscriber2 subscriber_2;
   Subscriber1 subscriber_1;
-  std::thread publisherThread(publisher);
   std::thread sub1Thread(subscriber_1);
   std::thread sub2Thread(subscriber_2);
+  std::thread publisherThread(publisher);
 
-//  std::this_thread::sleep_for(std::chrono::seconds(4));
   publisherThread.join();
   sub2Thread.join();
   sub1Thread.join();
-
-//  thread t1(Consumer1(), 2);
-//  t1.join();
-//
-//  std::thread th1(foo,4);
-//
-//  th1.join();
-
   return 0;
 }
 
